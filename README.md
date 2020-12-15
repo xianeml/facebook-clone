53149773e6c87820a3290e1bea5d62ab31ab982e
xianeml <xianemml@gmail.com>
2020-12-15 21:31:14 +0900

    ✨ feat: Make header

---
+++ README.md
@@ -0,0 +1,7 @@
+# ✨ facebook clone with React ✨
+
+**Tutorial Link** 👉 [https://youtu.be/B-kxUMHBxNo](https://youtu.be/B-kxUMHBxNo)
+
+### day1: make Header
+
+![md-Image/Untitled.png](md-Image/Untitled.png)
--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM naming convention
     <div className='app'>
-      <h1>Let's build the facebook clone!</h1>
+      <Header />
 
       {/* Header */}
 
