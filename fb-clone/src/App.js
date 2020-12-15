b40d4d68008f1e4d2601479c5169b400ce62f0f4
xianeml <xianemml@gmail.com>
2020-12-15 18:25:40 +0900

    🎉 begin a new fb-clone project!

---
+++ fb-clone/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className='app'>
+      <h1>Let's build the facebook clone!</h1>
+
+      {/* Header */}
+
+      {/* App body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
